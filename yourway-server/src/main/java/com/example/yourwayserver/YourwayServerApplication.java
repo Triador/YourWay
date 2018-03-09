1b45177fa92a0ee3815a4b8e142ca7d2e49a4088
Triador <triador.sub@yandex.ru>
2018-03-09 20:01:05 +0300

    initialize angular and spring modules

---
+++ yourway-server/src/main/java/com/example/yourwayserver/YourwayServerApplication.java
@@ -0,0 +1,12 @@
+package com.example.yourwayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourwayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YourwayServerApplication.class, args);
+	}
+}
