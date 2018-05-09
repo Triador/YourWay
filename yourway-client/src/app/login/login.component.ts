bb7f475450132403d76c46a8eb78d757f842a9ca
Triador <triador.sub@yandex.ru>
2018-05-10 01:10:29 +0300

    add login component implementation

--- yourway-client/src/app/login/login.component.ts
+++ yourway-client/src/app/login/login.component.ts
@@ -1,16 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { MatDialog } from '@angular/material';
+import { AuthService } from '../core/auth.service';
+import { TokenStorage } from '../core/token.storage';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
+  constructor(private router: Router, public dialog: MatDialog, private authService: AuthService, private token: TokenStorage) {
+  }
 
-  ngOnInit() {
+  username: string;
+  password: string;
+
+  login(): void {
+    this.authService.attemptAuth(this.username, this.password).subscribe(
+      data => {
+        this.token.saveToken(data.token);
+        this.router.navigate(['books']);
+      }
+    );
+  }
+
+  signup(): void {
+  	this.router.navigate(['signup']);
   }
 
 }
