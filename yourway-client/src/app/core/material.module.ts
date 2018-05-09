3791b5d3db3a2e72bd116e0b9aec0e0acfb07304
Triador <triador.sub@yandex.ru>
2018-05-10 01:13:37 +0300

    add new material modules

--- yourway-client/src/app/core/material.module.ts
+++ yourway-client/src/app/core/material.module.ts
@@ -1,8 +1,11 @@
-import {NgModule} from "@angular/core";
+import {NgModule} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {
+  MatButtonModule, MatCardModule, MatDialogModule, MatInputModule, MatTableModule, MatToolbarModule, MatProgressSpinnerModule
+} from '@angular/material';
 
 @NgModule({
-imports: [],
-exports: [],
+  imports: [CommonModule, MatToolbarModule, MatButtonModule, MatCardModule, MatInputModule, MatDialogModule, MatTableModule, MatProgressSpinnerModule],
+  exports: [CommonModule, MatToolbarModule, MatButtonModule, MatCardModule, MatInputModule, MatDialogModule, MatTableModule, MatProgressSpinnerModule],
 })
 export class CustomMaterialModule { }
