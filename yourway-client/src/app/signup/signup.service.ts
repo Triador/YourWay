eddd5427bb9d6f23cc68565a0875a437d33ab1c0
Triador <triador.sub@yandex.ru>
2018-05-10 01:08:00 +0300

    add sign up service

---
+++ yourway-client/src/app/signup/signup.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core'
+import { HttpClient, HttpHeaders } from '@angular/common/http'
+import { Router } from '@angular/router';
+
+import { User } from '../models/user.model'
+
+const httpOptins = {
+	headers: new HttpHeaders({ 'Content-Type': 'application/json'})
+};
+
+@Injectable()
+export class SignUpService {
+
+	constructor(private router: Router, private http: HttpClient) {}
+
+	private signupUrl = 'http://localhost:8080/signup'
+
+	public createUser(user) {
+		this.router.navigate(['login']);
+		return this.http.post(this.signupUrl, user);
+	}
+}
