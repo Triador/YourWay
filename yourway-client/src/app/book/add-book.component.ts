0d895c974bad11931418ba0648c3896140f180d6
Triador <triador.sub@yandex.ru>
2018-05-13 23:26:08 +0300

    add ngOnInit invocation after adding a book

--- yourway-client/src/app/book/add-book.component.ts
+++ yourway-client/src/app/book/add-book.component.ts
@@ -1,24 +1,27 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { MatTableDataSource } from '@angular/material';
 
 import { Book } from '../models/book.model';
 import { BookService } from './book.service';
+import { BookComponent } from './book.component'
 
 @Component({
 	selector: 'app-add-book',
-	templateUrl: './add-book.component.html'
+	templateUrl: './add-book.component.html',
+	styleUrls: ['add-book.component.css']
 })
 export class AddBookComponent {
 	book: Book = new Book();
 
-	constructor(private router: Router, private bookService: BookService) {
+	constructor(private router: Router, private bookService: BookService, private bookComponent: BookComponent) {
 
 	}
 
 	createBook(): void {
 		this.bookService.createBook(this.book)
 			.subscribe( data => {
-				alert("Book created successfully");
+				this.bookComponent.ngOnInit();
 			});
 	};
 }
