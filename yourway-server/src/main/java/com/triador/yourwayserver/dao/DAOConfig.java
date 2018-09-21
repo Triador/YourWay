206cfa9630eaf2bf248a048e279268769a741fb5
Triador <triador.sub@yandex.ru>
2018-09-22 01:46:44 +0300

    add DAOConfig with jdbc template and datasource configuration

---
+++ yourway-server/src/main/java/com/triador/yourwayserver/dao/DAOConfig.java
@@ -0,0 +1,38 @@
+package com.triador.yourwayserver.dao;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DAOConfig {
+
+    @Value("${spring.datasource.driver-class-name}")
+    private String databaseDriver;
+    @Value("${spring.datasource.username}")
+    private String databaseUsername;
+    @Value("${spring.datasource.password}")
+    private String databasePassword;
+    @Value("${spring.datasource.url}")
+    private String databaseUrl;
+
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        return new JdbcTemplate();
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(databaseDriver);
+        dataSource.setUsername(databaseUsername);
+        dataSource.setPassword(databasePassword);
+        dataSource.setUrl(databaseUrl);
+        return dataSource;
+    }
+
+}
