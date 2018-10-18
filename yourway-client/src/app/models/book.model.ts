6ae45300bd9623148627d689d0c3afb9fc8bed87
triador <triador.sub@yandex.ru>
2018-10-18 13:53:58 +0300

    return last changes

--- yourway-client/src/app/models/book.model.ts
+++ yourway-client/src/app/models/book.model.ts
@@ -1,11 +1,4 @@
 export class Book {
-	id: number;
-    russianTitle: string;
-    originTitle: string;
-    author: string;
-    pageAmount: number;
-    publicationYear: number;
-    isbn: string;
-    description: string;
-    imageLink: string;
+	id: string;
+	title: string;
 }
